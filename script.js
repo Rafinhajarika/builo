35dbb7561772d4f39513c49dbb688548b01e8be4
unknown <rafinhajarika@gmail.com>
2024-06-26 18:41:55 +0600

    updated

--- script.js
+++ script.js
@@ -37,13 +37,6 @@ var swiper = new Swiper(".mySwiper", {
 accordionItemHeaders.forEach(accordionItemHeader => {
    accordionItemHeader.addEventListener("click", event => {
     
-     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-//     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-//     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-//        currentlyActiveAccordionItemHeader.classList.toggle("active");
-//        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//      }
 
      accordionItemHeader.classList.toggle("active");
      const accordionItemBody = accordionItemHeader.nextElementSibling;
